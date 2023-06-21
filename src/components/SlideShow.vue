1ba79b1ada0f7701e7e7d18c321a847a23e73f32
Niklaswol <Niklaswollgarten@gmx.de>
2023-06-21 17:52:45 +0200

    Common Changes

--- src/components/SlideShow.vue
+++ src/components/SlideShow.vue
@@ -1,5 +1,5 @@
 <template>
-  <Splide class="mt-"
+  <Splide class="mt-4"
       :options="{ type  : 'fade', perPage: 1, gap: 0, padding: 0, rewind: true, autoplay: true, interval: 3000, arrows: false}"
       :tag="'section'" aria-label="My Favorite Images">
     <SplideSlide>
