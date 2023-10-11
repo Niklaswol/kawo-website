3917ffa6d175b0013865cc296f4445c054cc2dfa
Niklaswol <Niklaswollgarten@gmx.de>
2023-10-11 14:59:25 +0200

    changed navbar

--- src/components/NavbarMenu.vue
+++ src/components/NavbarMenu.vue
@@ -1,6 +1,9 @@
 <template>
-  <nav class="navbar is-transparent has-centered-logo-alt2" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand is-hidden-desktop">
+  <nav class="navbar is-transparent has-centered-menu" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      </a>
       <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
          data-target="navbarBasicExample">
         <span aria-hidden="true"></span>
@@ -10,23 +13,6 @@
     </div>
     <div id="navbarBasicExample" class="navbar-menu">
       <div class="navbar-start">
-      </div>
-      <div class="navbar-brand is-hidden-touch">
-        <a class="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-        </a>
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
-           data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div class="navbar-end">
-        <LocaleChanger class="locale-changer"/>
-      </div>
-      <hr>
-      <div class="navbar-start navbar-wide">
         <router-link class="navbar-item" to="/"> {{ $t("navbar.home") }}</router-link>
 
         <div class="navbar-item has-dropdown is-hoverable">
@@ -40,6 +26,9 @@
 
         <router-link class="navbar-item" to="/about-us"> {{ $t("navbar.aboutUs") }}</router-link>
         <router-link class="navbar-item" to="/contact"> {{ $t("navbar.contact") }}</router-link>
+      </div>
+      <div class="navbar-end">
+        <LocaleChanger class="locale-changer"/>
       </div>
     </div>
   </nav>
@@ -57,22 +46,10 @@ export default {
 </script>
 
 <style scoped>
-.navbar {
-  margin: 0 1rem 0 1rem;
-}
-
-.navbar-menu {
-  flex-wrap: wrap;
-}
-
-.navbar-start,
-.navbar-end {
-  flex: 1 1 0;
-}
-
-.navbar-wide.navbar-start {
-  flex: 1 1 100%;
+.navbar.has-centered-menu .navbar-start {
+  flex-grow: 1;
   justify-content: center;
+  margin: 10px;
 }
 
 .button {
